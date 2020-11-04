af8d89386f2669a6b688ee4c44a3ec9465796aaa
CSWILIK <cswilik@gmail.com>
2020-11-03 19:28:56 -0500

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bucky", 3, "m", "brown", "docile", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("kobe", 11, "f", "black", "docile", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("allen", 15, "m", "brown", "angry", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("rebecca", 12, "f", "red", "docile", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("paul", 3, "m", "red", "angry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("lucy", 6, "f", "brown", "docile", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("poopy", 1, "m", "white", "angrey", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "f", "brown", "docile", true);
